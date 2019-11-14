a65ad54ba5d52198ca5061791d1b476d7366e18b
yutailang0119 <muta.yutaro@gmail.com>
2019-11-14 14:00:52 +0900

    Fix xcode-find/main for CLI excute

--- Sources/xcode-find/main.swift
+++ Sources/xcode-find/main.swift
@@ -1,12 +1,41 @@
 import Foundation
-import XCFindKit
 
-do {
-    let finder: XcodeFinder = SpotlightXcodeFinder()
-
-    let xcodes = finder.findXcodes()
-    print(xcodes)
-
-    let xcode11_2 = finder.find("11")
-    print(xcode11_2)
+let arguments = Array(CommandLine.arguments.dropFirst())
+guard let option: String = arguments.first else {
+    let tool = CatchFailureTool(reason: "no command option given")
+    tool.run()
+    exit(1)
 }
+
+let tool: Tool
+
+switch option {
+case "-h", "--help":
+    if let argument = arguments.dropFirst().first {
+        tool = CatchFailureTool(reason: "invalid argument '\(argument)'")
+    } else {
+        tool = HelpTool()
+    }
+case "-a", "--all":
+    if let argument = arguments.dropFirst().first {
+        tool = CatchFailureTool(reason: "invalid argument '\(argument)'")
+    } else {
+        tool = FindAllTool()
+    }
+case "-p", "--print-version":
+    if let version = arguments.dropFirst().first {
+        tool = FindTool(version: version)
+    } else {
+        tool = CatchFailureTool(reason: "missing argument to '\(option)'")
+    }
+case "-v", "--version":
+    if let argument = arguments.dropFirst().first {
+        tool = CatchFailureTool(reason: "invalid argument '\(argument)'")
+    } else {
+        tool = VersionTool()
+    }
+default:
+    tool = CatchFailureTool(reason: "no command option given")
+}
+
+tool.run()
