71dac89d8607a239ef4076ff25bd73680cb3dbf7
yutailang0119 <muta.yutaro@gmail.com>
2019-11-14 13:59:04 +0900

    Add FindTool

---
+++ Sources/xcode-find/Tools/FindTool.swift
@@ -0,0 +1,19 @@
+import Foundation
+import XCFindKit
+
+struct FindTool: Tool {
+    private let version: String
+
+    init(version: String) {
+        self.version = version
+    }
+
+    func run() {
+        let finder = SpotlightXcodeFinder()
+        guard let xcode = finder.find(version) else {
+            print("Not Found selected version Xcode")
+            return
+        }
+        print(xcode.path.path)
+    }
+}
